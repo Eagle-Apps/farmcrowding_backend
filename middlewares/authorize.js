37f3a8de8182db44c88fcf5e1c4566a606b1d0a6
Meedo365 <83066257+Meedo365@users.noreply.github.com>
2023-03-01 11:53:07 +0100

    Update authorize.js

--- middlewares/authorize.js
+++ middlewares/authorize.js
@@ -1,48 +1,60 @@
-// const jwt = require("jsonwebtoken")
-// require("dotenv").config()
+const jwt = require("jsonwebtoken");
 const ErrorResponse = require("../utils/errorResponse");
+const User = require("../models/user");
 
 
-// const authorize = (req, res, next) => {
-//     let token;
-//     const authHeader = req.headers.authorization
+const isAdmin = (req, res, next) => {
+    const authHeader = req.headers.authorization
+    if (!authHeader || !authHeader.startsWith("Bearer")) {
+        return res.status(404).json({ success: false, msg: "Unauthorized User" })
+    }
+    try {
+        const token = req.header("Authorization").split(" ")[1]
+        if (!token) return res.status(400).json({ msg: "Invalid Authentication....." })
 
-//     if (authHeader && authHeader.startsWith("Bearer")) {
-//         token = authHeader.split(" ")[1]
-//     }
-//     if (req.cookies.token) {
-//         token = req.cookies.token
-//     }
-
-//     if (!token) return next(new ErrorResponse("unauthorize user", 401))
-
-//     const verified = jwt.verify(token, process.env.JWT_SECRET)
-
-//     req.user = verified
-
-//     next()
-
-// }
+        jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, async (err, user) => {
+            if (err) return res.status(400).json({ msg: "Invalid Authentication." })
+            let user_ = await User.findOne({ _id: user.id })
+            if (user_.role === "admin") {
+                req.user = user
+                next()
+            }
+            else {
+                return res.status(400).json({ msg: "User Not Authorised to Visit this Route." })
+            }
+        })
+    } catch (err) {
+        return res.status(500).json({ msg: err.message })
+    }
+};
 
 const authorize = (req, res, next) => {
     const authHeader = req.headers.authorization
     if (!authHeader || !authHeader.startsWith("Bearer")) {
-        return res.status(404).json({ success: false, msg: "unauthorize user" })
+        return res.status(404).json({ success: false, msg: "Unauthorized User" })
     }
-    const token = authHeader.split(" ")[1]
-    let verified = jwt.verify(token, process.env.SECRET)
-    req.user = verified
-    next()
+    try {
+        const token = req.header("Authorization").split(" ")[1]
+        if (!token) return res.status(400).json({ msg: "Invalid Authentication....." })
+
+        jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, user) => {
+            if (err) return res.status(400).json({ msg: "Invalid Authentication." })
+            req.user = user
+            next()
+        })
+    } catch (err) {
+        return res.status(500).json({ msg: "User Not Authorised !!!" })
+    }
 
 }
 
-const access = () => {
-    return (req, res, next) => {
-        if (!req.userId.role === "admin") {
-            return next(new ErrorResponse(`the ${req.userId.role} is not authorized to visit this route`, 403))
-        }
-        next()
-    }
-}
+// const access = () => {
+//     return (req, res, next) => {
+//         if (!req.userId.role === "admin") {
+//             return next(new ErrorResponse(`the ${req.userId.role} is not authorized to visit this route`, 403))
+//         }
+//         next()
+//     }
+// }
 
-module.exports = { authorize, access } 
+module.exports = { authorize, isAdmin } 
