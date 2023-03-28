ec5af1c7138f507ca96e860cdbbfec1ae8ce87e5
Meedo365 <83066257+Meedo365@users.noreply.github.com>
2023-03-28 06:14:50 +0100

    Update Payment.js

--- routes/endpoints/Payment.js
+++ routes/endpoints/Payment.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 const Payment = require('../../models/payment');
 const Flutterwave = require('flutterwave-node-v3');
 const open = require("open");
-const flw = new Flutterwave(process.env.FLW_PUBLIC_KEY, process.env.FLW_SECRET_KEY);
+// const flw = new Flutterwave(process.env.FLW_PUBLIC_KEY, process.env.FLW_SECRET_KEY);
 const User = require("../../models/user");
 // const got = require("got");
 const axios = require('axios');
@@ -88,86 +88,86 @@ let routes = (app) => {
     // });
 
 
-    app.post('/pay', async (req, res) => {
-        let user = await User.findOne({ _id: req.body.userId });
-        let payment = new Payment(req.body);
-        const payload = {
-            card_number: req.body.card_number,
-            cvv: req.body.card_cvv,
-            expiry_month: req.body.card_expiry_month,
-            expiry_year: req.body.card_expiry_year,
-            currency: 'NGN',
-            amount: req.body.amount,
-            email: user.email,
-            fullname: user.name,
-            phone_number: user.phone,
-            // Generate a unique transaction reference
-            tx_ref: payment._id,
-            // redirect_url: process.env.APP_BASE_URL + '/pay/redirect',
-            redirect_url: 'https://www.goal.com/en-ng',
-            enckey: process.env.FLW_ENCRYPTION_KEY
-        }
-        console.log(payment)
-        await payment.save()
-        const response = await flw.Charge.card(payload);
+    // app.post('/pay', async (req, res) => {
+    //     let user = await User.findOne({ _id: req.body.userId });
+    //     let payment = new Payment(req.body);
+    //     const payload = {
+    //         card_number: req.body.card_number,
+    //         cvv: req.body.card_cvv,
+    //         expiry_month: req.body.card_expiry_month,
+    //         expiry_year: req.body.card_expiry_year,
+    //         currency: 'NGN',
+    //         amount: req.body.amount,
+    //         email: user.email,
+    //         fullname: user.name,
+    //         phone_number: user.phone,
+    //         // Generate a unique transaction reference
+    //         tx_ref: payment._id,
+    //         // redirect_url: process.env.APP_BASE_URL + '/pay/redirect',
+    //         redirect_url: 'https://www.goal.com/en-ng',
+    //         enckey: process.env.FLW_ENCRYPTION_KEY
+    //     }
+    //     console.log(payment)
+    //     await payment.save()
+    //     const response = await flw.Charge.card(payload);
 
-        switch (response?.meta?.authorization?.mode) {
-            case 'pin':
-                const payload2 = {
-                    card_number: req.body.card_number,
-                    cvv: req.body.card_cvv,
-                    expiry_month: req.body.card_expiry_month,
-                    expiry_year: req.body.card_expiry_year,
-                    currency: 'NGN',
-                    amount: req.body.amount,
-                    email: user.email,
-                    fullname: user.name,
-                    phone_number: user.phone,
-                    // Generate a unique transaction reference
-                    tx_ref: payment._id,
-                    // redirect_url: process.env.APP_BASE_URL + '/pay/redirect',
-                    redirect_url: 'https://www.goal.com/en-ng',
-                    enckey: process.env.FLW_ENCRYPTION_KEY,
-                    authorization: {
-                        "mode": "pin",
-                        "pin": req.body.pin
-                    }
-                }
+    //     switch (response?.meta?.authorization?.mode) {
+    //         case 'pin':
+    //             const payload2 = {
+    //                 card_number: req.body.card_number,
+    //                 cvv: req.body.card_cvv,
+    //                 expiry_month: req.body.card_expiry_month,
+    //                 expiry_year: req.body.card_expiry_year,
+    //                 currency: 'NGN',
+    //                 amount: req.body.amount,
+    //                 email: user.email,
+    //                 fullname: user.name,
+    //                 phone_number: user.phone,
+    //                 // Generate a unique transaction reference
+    //                 tx_ref: payment._id,
+    //                 // redirect_url: process.env.APP_BASE_URL + '/pay/redirect',
+    //                 redirect_url: 'https://www.goal.com/en-ng',
+    //                 enckey: process.env.FLW_ENCRYPTION_KEY,
+    //                 authorization: {
+    //                     "mode": "pin",
+    //                     "pin": req.body.pin
+    //                 }
+    //             }
 
-                const response = await flw.Charge.card(payload2);
-                return res.send({ msg: response.data.processor_response, ref: response.data.flw_ref })
-            // case 'avs_noauth':
-            //     // Store the current payload
-            //     req.session.charge_payload = payload;
-            //     // Now we'll show the user a form to enter
-            //     // the requested fields (PIN or billing details)
-            //     req.session.auth_fields = response.meta.authorization.fields;
-            //     req.session.auth_mode = response.meta.authorization.mode;
-            //     return res.redirect('/pay/authorize');
-            // case 'redirect':
-            //     // Store the transaction ID
-            //     // so we can look it up later with the flw_ref
-            //     await redis.setAsync(`txref-${response.data.tx_ref}`, response.data.id);
-            //     // Auth type is redirect,
-            //     // so just redirect to the customer's bank
-            //     const authUrl = response.meta.authorization.redirect;
-            //     return res.redirect(authUrl);
-            // default:
-            //     // No authorization needed; just verify the payment
-            //     const transactionId = response.data.id;
-            //     const transaction = await flw.Transaction.verify({ id: transactionId });
-            //     if (transaction.data.status == "successful") {
-            //         return res.redirect('/payment-successful');
-            //     } else if (transaction.data.status == "pending") {
-            //         // Schedule a job that polls for the status of the payment every 10 minutes
-            //         transactionVerificationQueue.add({ id: transactionId });
-            //         return res.redirect('/payment-processing');
-            //     } else {
-            //         return res.redirect('/payment-failed');
-            //     }
-        }
+    //             const response = await flw.Charge.card(payload2);
+    //             return res.send({ msg: response.data.processor_response, ref: response.data.flw_ref })
+    //         // case 'avs_noauth':
+    //         //     // Store the current payload
+    //         //     req.session.charge_payload = payload;
+    //         //     // Now we'll show the user a form to enter
+    //         //     // the requested fields (PIN or billing details)
+    //         //     req.session.auth_fields = response.meta.authorization.fields;
+    //         //     req.session.auth_mode = response.meta.authorization.mode;
+    //         //     return res.redirect('/pay/authorize');
+    //         // case 'redirect':
+    //         //     // Store the transaction ID
+    //         //     // so we can look it up later with the flw_ref
+    //         //     await redis.setAsync(`txref-${response.data.tx_ref}`, response.data.id);
+    //         //     // Auth type is redirect,
+    //         //     // so just redirect to the customer's bank
+    //         //     const authUrl = response.meta.authorization.redirect;
+    //         //     return res.redirect(authUrl);
+    //         // default:
+    //         //     // No authorization needed; just verify the payment
+    //         //     const transactionId = response.data.id;
+    //         //     const transaction = await flw.Transaction.verify({ id: transactionId });
+    //         //     if (transaction.data.status == "successful") {
+    //         //         return res.redirect('/payment-successful');
+    //         //     } else if (transaction.data.status == "pending") {
+    //         //         // Schedule a job that polls for the status of the payment every 10 minutes
+    //         //         transactionVerificationQueue.add({ id: transactionId });
+    //         //         return res.redirect('/payment-processing');
+    //         //     } else {
+    //         //         return res.redirect('/payment-failed');
+    //         //     }
+    //     }
 
-    });
+    // });
 
 
     // The route where we send the user's auth details (Step 4)
@@ -209,28 +209,28 @@ let routes = (app) => {
 
 
     // The route where we validate and verify the payment (Steps 5 - 6)
-    app.post('/pay/validate', async (req, res) => {
-        const response = await flw.Charge.validate({
-            otp: req.body.otp,
-            flw_ref: req.body.ref
-        });
+    // app.post('/pay/validate', async (req, res) => {
+    //     const response = await flw.Charge.validate({
+    //         otp: req.body.otp,
+    //         flw_ref: req.body.ref
+    //     });
 
-        if (response.data.status === 'successful' || response.data.status === 'pending') {
-            // Verify the payment
-            const transactionId = response.data.id;
-            const transaction = await flw.Transaction.verify({ id: transactionId });
+    //     if (response.data.status === 'successful' || response.data.status === 'pending') {
+    //         // Verify the payment
+    //         const transactionId = response.data.id;
+    //         const transaction = await flw.Transaction.verify({ id: transactionId });
 
-            if (transaction.data.status == "successful") {
-                return res.send({ msg: "payment confirmed" });
-            } else if (transaction.data.status == "pending") {
-                // Schedule a job that polls for the status of the payment every 10 minutes
-                transactionVerificationQueue.add({ id: transactionId });
-                return res.redirect('/payment-processing');
-            }
-        }
+    //         if (transaction.data.status == "successful") {
+    //             return res.send({ msg: "payment confirmed" });
+    //         } else if (transaction.data.status == "pending") {
+    //             // Schedule a job that polls for the status of the payment every 10 minutes
+    //             transactionVerificationQueue.add({ id: transactionId });
+    //             return res.redirect('/payment-processing');
+    //         }
+    //     }
 
-        // return res.redirect('/payment-failed');
-    });
+    //     // return res.redirect('/payment-failed');
+    // });
 
     // // Our redirect_url. For 3DS payments, Flutterwave will redirect here after authorization,
     // // and we can verify the payment (Step 6)
